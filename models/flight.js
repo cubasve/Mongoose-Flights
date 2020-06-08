81619de8d862da1cddc35d99b35048c3fbaf20be
Eva Cubas Vasquez <cubasve@ECV-MacBook-Pro.local>
2020-06-07 23:16:09 -0400

    working on defaults

--- models/flight.js
+++ models/flight.js
@@ -3,9 +3,22 @@ const Schema = mongoose.Schema;
 
 const flightSchema = new Schema ({ //define schema
     airline: String,
-    airport: String,
-    flightNo: Number,
-    departs: Date,
+
+    airport: {type: String, default: 'DEN'},
+
+    flightNo: {type: Number, required: true, 
+        default: function() {
+            return new Date().getFullYear();
+        },
+        min: 10,
+        max: 9999,
+    },
+    
+    departs: {type: Date, 
+        default: function() {
+            return new Date().getFullYear();
+        },
+    },
 });
 
 module.exports = mongoose.model('Flight', flightSchema); //compile schema into model
